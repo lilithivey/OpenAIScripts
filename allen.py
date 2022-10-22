fc0e366f00982e95eff7e51167816f1bc597998d
lilithivey <105260730+lilithivey@users.noreply.github.com>
2022-10-21 23:46:10 -0400

    made it for only five jokes per run

--- allen.py
+++ allen.py
@@ -5,13 +5,12 @@ import os
 print("The Allenator")
 print("################################################################################")
 print(" ")
-times = input("how many? ")
 print("sending...")
 openai.api_key = "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"
 
 response = openai.Completion.create(
     model="text-davinci-002",
-    prompt="we have a mutual coworker named Allen, and we make jokes about him.  /n/n/n you will Create a list that contains" + times+" jokes in the style of a Comedy Central Roasting, predominately about his weight. old age, or inability to stay married. jokes must be sarcastic but not hurtful./n/n/nExamples:/nAllen might be late coming in. They're still greasing him up so he can fit through the doorway./n/n'I hope Allen never passes out upstairs. We can't fit a forklift up there to get him.'",
+    prompt="we have a mutual coworker named Allen, and we make jokes about him.  /n/n/n you will Create a list that contains five jokes (one per line) in the style of a Comedy Central Roasting, predominately about his weight. old age, or inability to stay married. jokes must be sarcastic but not hurtful./n/n/nExamples:/nAllen might be late coming in. They're still greasing him up so he can fit through the doorway./n/n'I hope Allen never passes out upstairs. We can't fit a forklift up there to get him.'",
     temperature=1,
     max_tokens=512,
     top_p=.05,
